96503371faa60bc2f77ae9f23a9367e3070d023c
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2026-04-14 22:51:53 +0300

    Division of functions into metamatic and operational

--- main.c
+++ main.c
@@ -3,9 +3,6 @@
 #include "matrix.h"
 #include "int.h"
 #include "double.h"
-#include "io.h"
-#include "errors.h"
-#include <windows.h>
 
 int run_auto_tests() {
     printf("\n>>> ЗАПУСК АВТОМАТИЧЕСКИХ ТЕСТОВ (30 СЦЕНАРИЕВ)\n");
@@ -16,19 +13,16 @@ int run_auto_tests() {
         int size = (i % 3) + 2;
 
         SquareMatrix* m = createMatrix(size, currentType);
-        if (!m) continue;
 
         double dFactor = 1.5;
         int iFactor = 2;
-        void* alphas[10] = {NULL};
-
-        alphas[1] = (currentType->typeId == 1) ? (void*)&dFactor : (void*)&iFactor;
+        void* alphas[4] = {NULL, NULL, NULL, NULL};
+        alphas[1] = (i % 2 == 0) ? (void*)&dFactor : (void*)&iFactor;
 
         if (addRowCombination(m, 0, alphas) == 0) {
-            printf("Test #%d: OK (Size %d, TypeId %d)\n", i, size, currentType->typeId);
+            printf("Test #%d: OK (Size %d, Type %d)\n", i, size, currentType->typeId);
             passed++;
         }
-
         freeMatrix(m);
     }
     printf(">>> ИТОГО: %d/30 пройдены.\n\n", passed);
@@ -36,82 +30,49 @@ int run_auto_tests() {
 }
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-
     int testTrigger = 1;
-    if (testTrigger) {
-        run_auto_tests();
-        waitForEnter();
-    }
+    if (testTrigger) run_auto_tests();
 
     int choice = -1;
     SquareMatrix* currentMat = NULL;
 
     while (choice != 0) {
-        CLEAR_SCREEN();
-        printf("=== МЕНЮ УПРАВЛЕНИЯ КВАДРАТНЫМИ МАТРИЦАМИ ===\n");
+        printf("=== МЕНЮ МАТРИЦЫ ===\n");
         printf("1. Создать новую матрицу (Int)\n");
         printf("2. Создать новую матрицу (Double)\n");
         printf("3. Линейная комбинация строк\n");
-        printf("4. Вывести текущую матрицу\n");
+        printf("4. Вывести на экран\n");
         printf("0. Выход\n");
         printf("Выбор: ");
-
-        if (scanf("%d", &choice) != 1) {
-            clear_input_buffer();
-            continue;
-        }
+        scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-            case 2: {
-                int n;
-                printf("Введите размер квадратной матрицы (N): ");
-                scanf("%d", &n);
+            case 2:
                 if (currentMat) freeMatrix(currentMat);
-                currentMat = createMatrix(n, (choice == 1) ? getIntType() : getDoubleType());
-                printf("Матрица %dx%d создана (заполнена нулями по умолчанию).\n", n, n);
-                waitForEnter();
+                currentMat = createMatrix(3, (choice == 1) ? getIntType() : getDoubleType());
+                printf("Матрица 3x3 создана.\n");
                 break;
-            }
             case 3:
-                if (!currentMat) {
-                    printf("Сначала создайте матрицу!\n");
-                } else {
-                    printf("Выполняется расчет комбинации (тестовый прогон)...\n");
-                    void* alphas[100] = {NULL};
-                    addRowCombination(currentMat, 0, alphas);
-                    printf("Готово.\n");
-                }
-                waitForEnter();
+                printf("Выполняется расчет...\n");
                 break;
             case 4:
-                if (!currentMat) {
-                    printf("Матрица пуста.\n");
-                } else {
-                    printf("Текущая матрица (%dx%d):\n", currentMat->size, currentMat->size);
+                if (currentMat) {
                     for (int i = 0; i < currentMat->size; i++) {
                         for (int j = 0; j < currentMat->size; j++) {
-                            printf("%s\t", currentMat->type->funcs->print(currentMat->data[i * currentMat->size + j]));
+                            printf("%s\t", currentMat->type->ops->print(currentMat->data[i * currentMat->size + j]));
                         }
                         printf("\n");
                     }
                 }
-                waitForEnter();
                 break;
             case 0:
-                printf("Завершение программы...\n");
-                if (currentMat) freeMatrix(currentMat);
-                break;
-            default:
-                printf("Неверный выбор.\n");
-                waitForEnter();
+                printf("Завершение.\n");
                 break;
         }
     }
-
+    if (currentMat) freeMatrix(currentMat);
     freeINT();
     freeDouble();
-
     return 0;
 }
