e86695d2fe122229b2b897cfa0b2e4fc8a94f900
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2026-04-14 22:58:11 +0300

    Type checking

--- int.c
+++ int.c
@@ -4,16 +4,8 @@
 
 static IType* INT_TYPE_INSTANCE = NULL;
 
-int intAdd(const void* a, const void* b, void* res) {
-    *(int*)res = *(int*)a + *(int*)b;
-    return 0;
-}
-
-int intMul(const void* a, const void* b, void* res) {
-    *(int*)res = (*(int*)a) * (*(int*)b);
-    return 0;
-}
-
+int intAdd(const void* a, const void* b, void* res) { *(int*)res = *(int*)a + *(int*)b; return 0; }
+int intMul(const void* a, const void* b, void* res) { *(int*)res = (*(int*)a) * (*(int*)b); return 0; }
 char* intPrint(const void* a) {
     static char buf[32];
     sprintf(buf, "%d", *(int*)a);
@@ -25,10 +17,8 @@ IType* getIntType() {
         INT_TYPE_INSTANCE = malloc(sizeof(IType));
         INT_TYPE_INSTANCE->math = malloc(sizeof(TypeMath));
         INT_TYPE_INSTANCE->ops = malloc(sizeof(TypeOps));
-
         INT_TYPE_INSTANCE->size = sizeof(int);
-        INT_TYPE_INSTANCE->typeId = 0;
-
+        INT_TYPE_INSTANCE->typeId = 0; // ID для INT
         INT_TYPE_INSTANCE->math->add = intAdd;
         INT_TYPE_INSTANCE->math->mul = intMul;
         INT_TYPE_INSTANCE->ops->print = intPrint;
