09b2e1a410dc83f96361b2510bb798ca6b414748
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2026-04-14 23:01:02 +0300

    Type checking

--- double.c
+++ double.c
@@ -5,24 +5,32 @@
 static IType* DOUBLE_TYPE_INSTANCE = NULL;
 
 int doubleAdd(const void* a, const void* b, void* res) {
+    if (!a || !b || !res) return 100;
     *(double*)res = *(double*)a + *(double*)b;
     return 0;
 }
 
 int doubleMul(const void* a, const void* b, void* res) {
+    if (!a || !b || !res) return 100;
     *(double*)res = (*(double*)a) * (*(double*)b);
     return 0;
 }
 
 char* doublePrint(const void* a) {
-    static char buf[32];
+    static char buf[64];
     sprintf(buf, "%.3f", *(double*)a);
     return buf;
+}
+
+int doubleFree(void* a) {
+    return 0;
 }
 
 IType* getDoubleType() {
     if (DOUBLE_TYPE_INSTANCE == NULL) {
         DOUBLE_TYPE_INSTANCE = malloc(sizeof(IType));
+        if (!DOUBLE_TYPE_INSTANCE) return NULL;
+
         DOUBLE_TYPE_INSTANCE->math = malloc(sizeof(TypeMath));
         DOUBLE_TYPE_INSTANCE->ops = malloc(sizeof(TypeOps));
 
@@ -31,16 +39,17 @@ IType* getDoubleType() {
 
         DOUBLE_TYPE_INSTANCE->math->add = doubleAdd;
         DOUBLE_TYPE_INSTANCE->math->mul = doubleMul;
+
         DOUBLE_TYPE_INSTANCE->ops->print = doublePrint;
-        DOUBLE_TYPE_INSTANCE->ops->free = NULL;
+        DOUBLE_TYPE_INSTANCE->ops->free = doubleFree;
     }
     return DOUBLE_TYPE_INSTANCE;
 }
 
 void freeDouble() {
     if (DOUBLE_TYPE_INSTANCE) {
-        free(DOUBLE_TYPE_INSTANCE->math);
-        free(DOUBLE_TYPE_INSTANCE->ops);
+        if (DOUBLE_TYPE_INSTANCE->math) free(DOUBLE_TYPE_INSTANCE->math);
+        if (DOUBLE_TYPE_INSTANCE->ops) free(DOUBLE_TYPE_INSTANCE->ops);
         free(DOUBLE_TYPE_INSTANCE);
         DOUBLE_TYPE_INSTANCE = NULL;
     }
