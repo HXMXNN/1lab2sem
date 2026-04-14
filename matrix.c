9c9fdc72b01c706bf05bb9d72bdd0b9d9b5c5c75
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2026-04-14 22:47:32 +0300

    Division of functions into metamatic and operational

--- matrix.c
+++ matrix.c
@@ -1,6 +1,5 @@
 #include "matrix.h"
 #include <stdlib.h>
-#include <string.h>
 
 SquareMatrix* createMatrix(int n, IType* type) {
     SquareMatrix* m = malloc(sizeof(SquareMatrix));
@@ -10,30 +9,22 @@ SquareMatrix* createMatrix(int n, IType* type) {
     m->data = malloc(sizeof(void*) * n * n);
     for (int i = 0; i < n * n; i++) {
         m->data[i] = malloc(type->size);
-        memset(m->data[i], 0, type->size);
     }
     return m;
 }
 
-void freeMatrix(SquareMatrix* m) {
-    if (!m) return;
-    for (int i = 0; i < m->size * m->size; i++) {
-        free(m->data[i]);
-    }
-    free(m->data);
-    free(m);
-}
-
 int addRowCombination(SquareMatrix* m, int targetRow, void** alphas) {
     if (!m || targetRow < 0 || targetRow >= m->size) return 200;
+
     void* scaled = malloc(m->type->size);
     int n = m->size;
 
     for (int j = 0; j < n; j++) {
         for (int k = 0; k < n; k++) {
             if (k == targetRow || alphas[k] == NULL) continue;
-            m->type->funcs->mul(m->data[k * n + j], alphas[k], scaled);
-            m->type->funcs->add(m->data[targetRow * n + j], scaled, m->data[targetRow * n + j]);
+
+            m->type->math->mul(m->data[k * n + j], alphas[k], scaled);
+            m->type->math->add(m->data[targetRow * n + j], scaled, m->data[targetRow * n + j]);
         }
     }
     free(scaled);
