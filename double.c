fbaf285d1b912c28a2c50de3ee4e97c5d35558df
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2026-04-14 22:51:06 +0300

    Division of functions into metamatic and operational

--- double.c
+++ double.c
@@ -8,10 +8,12 @@ int doubleAdd(const void* a, const void* b, void* res) {
     *(double*)res = *(double*)a + *(double*)b;
     return 0;
 }
+
 int doubleMul(const void* a, const void* b, void* res) {
     *(double*)res = (*(double*)a) * (*(double*)b);
     return 0;
 }
+
 char* doublePrint(const void* a) {
     static char buf[32];
     sprintf(buf, "%.3f", *(double*)a);
@@ -21,19 +23,24 @@ char* doublePrint(const void* a) {
 IType* getDoubleType() {
     if (DOUBLE_TYPE_INSTANCE == NULL) {
         DOUBLE_TYPE_INSTANCE = malloc(sizeof(IType));
-        DOUBLE_TYPE_INSTANCE->funcs = malloc(sizeof(TypeFuncs));
+        DOUBLE_TYPE_INSTANCE->math = malloc(sizeof(TypeMath));
+        DOUBLE_TYPE_INSTANCE->ops = malloc(sizeof(TypeOps));
+
         DOUBLE_TYPE_INSTANCE->size = sizeof(double);
         DOUBLE_TYPE_INSTANCE->typeId = 1;
-        DOUBLE_TYPE_INSTANCE->funcs->add = doubleAdd;
-        DOUBLE_TYPE_INSTANCE->funcs->mul = doubleMul;
-        DOUBLE_TYPE_INSTANCE->funcs->print = doublePrint;
+
+        DOUBLE_TYPE_INSTANCE->math->add = doubleAdd;
+        DOUBLE_TYPE_INSTANCE->math->mul = doubleMul;
+        DOUBLE_TYPE_INSTANCE->ops->print = doublePrint;
+        DOUBLE_TYPE_INSTANCE->ops->free = NULL;
     }
     return DOUBLE_TYPE_INSTANCE;
 }
 
 void freeDouble() {
     if (DOUBLE_TYPE_INSTANCE) {
-        free(DOUBLE_TYPE_INSTANCE->funcs);
+        free(DOUBLE_TYPE_INSTANCE->math);
+        free(DOUBLE_TYPE_INSTANCE->ops);
         free(DOUBLE_TYPE_INSTANCE);
         DOUBLE_TYPE_INSTANCE = NULL;
     }
