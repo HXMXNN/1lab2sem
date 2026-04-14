e1e922dfc5514c04879f9af85ef89d771bfeed31
Maxim Pantsa (HXMXNN) <pantsa00@bk.ru>
2026-04-14 22:45:30 +0300

    Division of functions into metamatic and operational

--- itype.h
+++ itype.h
@@ -6,14 +6,16 @@
 typedef struct {
     int (*add)(const void* a, const void* b, void* res);
     int (*mul)(const void* a, const void* b, void* res);
+} TypeMath;
+
+typedef struct {
     char* (*print)(const void* a);
     int (*free)(void* a);
-} TypeFuncs;
+} TypeOps;
 
 typedef struct {
     size_t size;
     int typeId;
-    TypeFuncs *funcs;
+    TypeMath *math;
+    TypeOps *ops;
 } IType;
-
-#endif
